eb3416e5112e8ffcd6bd206b1d152904920b33dc
maximthomas <maxim.thomas@gmail.com>
2025-10-02 12:28:01 +0300

    style user form

--- src/lib/components/DefaultUserForm.tsx
+++ src/lib/components/DefaultUserForm.tsx
@@ -7,8 +7,7 @@ const DefaultUserForm: UserForm = ({ userAuthData, userService }) => {
     const [userData, setUserData] = useState<UserData | null>(null);
 
     const onSave = (data: UserData) => {
-        console.log("Saving user data", data);
-        // Implement save logic here, e.g., call userService to update user data
+        console.log("Saving user data", data); //TODO
     };
 
     useEffect(() => {
@@ -29,55 +28,56 @@ const DefaultUserForm: UserForm = ({ userAuthData, userService }) => {
     };
 
     return (
+        <>
+        <h2>User Profile</h2>
         <form
             onSubmit={e => {
                 e.preventDefault();
                 onSave(userData);
-            }}
-            style={{ display: "flex", flexDirection: "column", gap: "1em", maxWidth: 500 }}
-        >
-            <label>
-                Username:
-                <input
+            }}>
+            <div className="form-group">
+                <label htmlFor="username">Username:</label>
+                <input name="username"
                     type="text"
                     value={userData.username} readOnly={true}
                 />
-            </label>
-            <label>
-                First Name:
-                <input
+            </div>
+            <div className="form-group">
+                <label htmlFor="givenName">First Name:</label>
+                <input name="givenName"
                     type="text"
                     value={userData.givenName ? userData.givenName[0] : ""}
                     onChange={e => handleChange("givenName", e.target.value)}
                 />
-            </label>
-            <label>
-                Last Name:
-                <input
+            </div>
+            <div className="form-group">
+                <label htmlFor="sn">Last Name:</label>
+                <input name="sn"
                     type="text"
                     value={userData.sn ? userData.sn[0] : ""}
                     onChange={e => handleChange("sn", e.target.value)}
                 />
-            </label>
-            <label>
-                Mail:
-                <input
+            </div>
+            <div className="form-group">
+                <label htmlFor="mail">Mail:</label>
+                <input name="mail"
                     type="email"
                     value={userData.mail ? userData.mail[0] : ""}
                     onChange={e => handleChange("mail", e.target.value)}
                 />
-            </label>
-            <label>
-                Phone number:
-                <input
+            </div>
+            <div className="form-group">
+                <label htmlFor="mail">Phone number:</label>
+                <input name="telephoneNumber"
                     type="tel"
                     value={userData.telephoneNumber ? userData.telephoneNumber[0] : ""}
                     onChange={e => handleChange("telephoneNumber", e.target.value)}
                 />
-            </label>
-
-            <input type="submit" value="Save" />
-        </form>);
+            </div>
+            <div className="button-group">
+                <input className="primary-button" type="submit" value="Save" />
+            </div>
+        </form></>);
 }
 
 export default DefaultUserForm;    
